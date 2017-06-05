d2c3c26b50c8aee378d330524734b85042c5de29
HOME <HOME@192.168.0.6>
2017-06-05 19:14:33 +0530

    ServletController program ... BY T.SUKANYA

--- src/calculator/Servletcontroller.java
+++ src/calculator/Servletcontroller.java
@@ -26,22 +26,45 @@ import javax.servlet.http.HttpSession;
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException 
 	{
-		// Create an object of BasicCalc class
+		BasicCalc det=new BasicCalc(); // Create an object of BasicCalc class
 		
-		//get parameter with req.getparameter() method and 
-		
-		// set the values with set parameter into variable a, b
-		
-		// get parameter operation 
-		
-	//	switch (operation)		
-		{
-		//write switch cases for calling different method of operations
-		}
-		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
-		rd.forward(req, resp); 
-		} 
+		double a=Double.valueOf(req.getParameter("a")); //get parameter with req.getparameter() method and 
+		double b=Double.valueOf(req.getParameter("b"));
+		det.setA(a);// set the values with set parameter into variable a, b
+		det.setB(b);
+		String operation=req.getParameter("operation");// get parameter operation 
 	
+		switch (operation)		
+		{
+			
+			case "Add": 
+				c=det.add();
+		System.out.println("c");
+		break;
+		
+		case "Substract": 
+			c=det.subtract();
+	System.out.println("c");
+	break;
+	
+case "Multiply":
+	c=det.multiply();
+System.out.println("c");
+break;
+
+case "Divide": 
+	c=det.divide();
+System.out.println("c");
+break;
+default:System.out.println("wrong choice");
+}
+		req.setAttribute("answer",c);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+		
+		rd.forward(req, resp); 
+		
+	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException {
